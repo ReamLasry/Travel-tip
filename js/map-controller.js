89fcd5b95622dad7a5b4c358dd2d8c1394f4173f
ReamLasry <ream794613@gmail.com>
2021-02-17 16:36:54 +0200

    map click update

--- js/map-controller.js
+++ js/map-controller.js
@@ -43,11 +43,23 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
                 })
             console.log('Map!', gMap);
 
+
+            // 
+            const myLatlng = { lat: -25.363, lng: 131.044 };
+            const map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 4,
+                center: myLatlng,
+            });
+            // Create the initial InfoWindow.
+            let infoWindow = new google.maps.InfoWindow({
+                content: "Click the map to get Lat/Lng!",
+                position: myLatlng,
+            });
+            infoWindow.open(map);
+            // Configure the click listener.
             map.addListener("click", (mapsMouseEvent) => {
-                console.log('i am clicking on the map');
                 // Close the current InfoWindow.
                 infoWindow.close();
-
                 // Create a new InfoWindow.
                 infoWindow = new google.maps.InfoWindow({
                     position: mapsMouseEvent.latLng,
@@ -57,6 +69,7 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
                 );
                 infoWindow.open(map);
             });
+            // 
         })
 }
 
